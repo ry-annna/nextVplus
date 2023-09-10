48d03ece1ae77b95ec27560ea966455431463911
SuperNayr135 <ryanadytia219@gmail.com>
2023-09-11 01:03:04 +0700

    afaf

--- src/pages/demoAkun.jsx
+++ src/pages/demoAkun.jsx
@@ -1,54 +1,66 @@
 "use client";
 
-import { Button, Checkbox, Label, TextInput } from "flowbite-react";
+import { Label } from "flowbite-react";
 
 const DemoAkunPage = () => {
   return (
     <div>
-      <section className="bg-[#3a336f] h-screen">
-        <div className="xl:w-[1140px] mx-auto xl:py-16 max-md:mx-4 md:max-lg:mx-10 max-md:pt-4 md:max-lg:pt-10 ">
-          <div className="border-4 border-[#f1c50e] flex justify-center rounded-3xl xl:p-[30px] max-md:p-[15px] md:max-lg:p-[15px]">
-            <p className="text-[16px] font-Open_Sans text-white">
+      <section className="bg-[#3a336f] h-full pb-10">
+        <div className="xl:w-[1140px] mx-auto xl:py-16 max-xl:mx-4 md:max-lg:mx-10 max-md:pt-4 md:pt-10 ">
+          <div className="border-4 border-[#f1c50e] flex justify-center rounded-3xl xl:p-[30px] max-md:p-[15px] md:max-xl:p-[15px]">
+            <p className="lg:text-[16px] max-md:text-[12px] font-Open_Sans text-white">
               Dengan dana virtual, asah skill trading anda dengan simulasi di
               akun demo dan memastikan kesiapan anda saat membuka Akun Real.
             </p>
           </div>
-          {/* <div className="flex flex-col items-center justify-center mt-10">
+          <div className="flex flex-col items-center justify-center mt-10">
             <h1 className="uppercase text-[36px] font-Open_Sans text-white font-bold mb-5">
               Demo Akun
             </h1>
-            <div className="bg-white p-[30px] rounded-2xl w-[711px]">
+            <div className="bg-white p-[30px] rounded-2xl xl:w-[711px]">
               <form className="flex flex-col w-full gap-4">
                 <div>
                   <div className="block mb-2">
-                    <Label htmlFor="nama" value="NAMA" />
+                    <Label
+                      htmlFor="nama"
+                      value="NAMA"
+                      className="text-[16px]"
+                    />
                   </div>
 
                   <input
                     type="text"
                     id="nama"
                     placeholder="Masukan nama Anda"
-                    className="w-full px-3 py-2 rounded-lg placeholder:opacity-50 placeholder:italic focus:outline-none border-1 border-slate-300 focus:border-[#f1c50e] focus:ring-[#f1c50e] focus:ring-2"
+                    className="w-full px-3 py-2 rounded-lg placeholder:opacity-50 placeholder:italic max-xl:placeholder:text-[13px] focus:outline-none border-1 border-slate-300 focus:border-[#f1c50e] focus:ring-[#f1c50e] focus:ring-2"
                     required
                   />
                 </div>
-                <div className="flex gap-2">
-                  <div className="w-1/2">
+                <div className="flex gap-2 max-sm:flex-col">
+                  <div className="w-1/2 max-sm:w-full">
                     <div className="block mb-2">
-                      <Label htmlFor="telepon" value="NOMOR TELEPON" />
+                      <Label
+                        htmlFor="telepon"
+                        value="NOMOR TELEPON"
+                        className="text-[16px]"
+                      />
                     </div>
 
                     <input
                       id="telepon"
                       required
                       type="text"
-                      placeholder="Masukan nomor telepon yang valid"
-                      className="w-full px-3 py-2 rounded-lg placeholder:opacity-50 placeholder:italic focus:outline-none border-1 border-slate-300 focus:border-[#f1c50e] focus:ring-[#f1c50e] focus:ring-2"
+                      placeholder="Masukan No. telepon yang valid"
+                      className="w-full px-3 py-2 rounded-lg placeholder:opacity-50 placeholder:italic max-xl:placeholder:text-[13px] focus:outline-none border-1 border-slate-300 focus:border-[#f1c50e] focus:ring-[#f1c50e] focus:ring-2"
                     />
                   </div>
-                  <div className="w-1/2">
+                  <div className="w-1/2 max-sm:w-full">
                     <div className="block mb-2">
-                      <Label htmlFor="email" value="EMAIL" />
+                      <Label
+                        htmlFor="email"
+                        value="EMAIL"
+                        className="text-[16px]"
+                      />
                     </div>
 
                     <input
@@ -56,12 +68,12 @@ const DemoAkunPage = () => {
                       required
                       type="email"
                       placeholder="Masukan email yang aktif"
-                      className="w-full px-3 py-2 rounded-lg placeholder:opacity-50 placeholder:italic border-1 border-slate-300 focus:outline-none focus:border-[#f1c50e] focus:ring-[#f1c50e] focus:ring-2"
+                      className="w-full px-3 py-2 rounded-lg placeholder:opacity-50 placeholder:italic max-xl:placeholder:text-[13px] border-1 border-slate-300 focus:outline-none focus:border-[#f1c50e] focus:ring-[#f1c50e] focus:ring-2"
                     />
                   </div>
                 </div>
 
-                <div className="flex items-center gap-2">
+                {/* <div className="flex items-center gap-2">
                   <Checkbox id="agree" />
                   <Label className="flex" htmlFor="agree">
                     <p>I agree with the</p>
@@ -72,7 +84,7 @@ const DemoAkunPage = () => {
                       <p>terms and conditions</p>
                     </LinkComponent>
                   </Label>
-                </div>
+                </div> */}
 
                 <button
                   type="submit"
@@ -82,13 +94,13 @@ const DemoAkunPage = () => {
                 </button>
               </form>
             </div>
-          </div> */}
+          </div>
         </div>
       </section>
-      {/* <section className="bg-[#f1c50e] h-full">
-        <div className="w-[1140px] flex justify-center mx-auto">
-          <div className="w-[570px] h-[400px] p-[30px]">
-            <h1 className="text-[60px] font-Montserrat text-black font-bold">
+      <section className="bg-[#f1c50e] h-full">
+        <div className="xl:w-[1140px] flex justify-center mx-auto max-md:flex-col">
+          <div className="xl:w-[570px] md:w-1/2 xl:h-[400px] md:p-[30px] max-md:px-[30px] max-md:pt-[30px]">
+            <h1 className="xl:text-[60px] max-xl:text-[35px] font-Montserrat text-black font-bold">
               Lisensi Broker
             </h1>
             <h3 className="text-[16px] font-Open_Sans text-black italic font-bold">
@@ -104,21 +116,21 @@ const DemoAkunPage = () => {
               <h2 className="text-[24px] font-Open_Sans text-black font-bold">
                 Hubungi Kami
               </h2>
-              <p className="italic text-[15px] font-Open_Sans text-black leading-8">
+              <p className="italic text-[15px] max-xl:text-[12px] font-Open_Sans text-black xl:leading-8">
                 Menara Rajawali Lantai 18, Jl. DR. Ide Anak Agung Gde Agung
                 No.5, RT.5/RW.2, Kuningan, Kuningan Tim., Kecamatan Setiabudi,
                 Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12950
               </p>
             </div>
           </div>
-          <div className="w-[570px] h-[400px] p-[30px]">
-            <h1 className="text-[60px] font-Montserrat text-black font-bold">
+          <div className="xl:w-[570px] md:w-1/2 xl:h-[400px] p-[30px]">
+            <h1 className="xl:text-[60px] max-xl:text-[35px] font-Montserrat text-black font-bold">
               Disclaimer
             </h1>
             <h3 className="text-[16px] font-Open_Sans text-black italic font-bold">
               Pemberitahuan Resiko
             </h3>
-            <p className="text-[16px] font-Open_Sans leading-loose mt-[10px]">
+            <p className="xl:text-[16px] max-xl:text-[12px] font-Open_Sans leading-loose mt-[10px]">
               Produk dengan leverage memiliki tingkat risiko yang tinggi
               terhadap modal yang anda investasikan dan disarankan hanya
               menggunakan dana yang mampu anda tanggung apabila terjadi
@@ -128,15 +140,15 @@ const DemoAkunPage = () => {
             </p>
           </div>
         </div>
-      </section> */}
-      {/* <footer className="h-[95px] bg-[#333333] flex justify-center flex-col items-center">
-        <p className="text-[14px] font-Open_Sans text-white">
+      </section>
+      <footer className="h-[95px] bg-[#333333] flex justify-center flex-col items-center">
+        <p className="sm:text-[14px] max-sm:text-[10px] font-Open_Sans text-white">
           Associate with PT Victory International Futures Platinum 8
         </p>
-        <p className="text-[14px] font-Open_Sans text-[#f1c50e]">
+        <p className="sm:text-[14px] max-sm:text-[10px] font-Open_Sans text-[#f1c50e]">
           Copyright © 2​020 vplus.id. All rights reserved.
         </p>
-      </footer> */}
+      </footer>
     </div>
   );
 };
