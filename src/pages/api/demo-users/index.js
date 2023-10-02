463b427bcc8719243e240c6055b66822e7d94ada
SuperNayr135 <ryanadytia219@gmail.com>
2023-10-02 20:46:13 +0700

    afaf

--- src/pages/api/demo-users/index.js
+++ src/pages/api/demo-users/index.js
@@ -1,7 +1,22 @@
 const UsersModel = require("./models/demo-akun.js");
 
 export default async function handler(req, res) {
-  if (req.method === "GET") {
+  if (req.method === "POST") {
+    const { body } = req;
+    try {
+      await UsersModel.createNewUser(body);
+      res.status(201).json({
+        status: 201,
+        message: "CREATE new user sukses",
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        message: "CREATE new user gagal",
+        serverMessage: error,
+      });
+    }
+  } else if (req.method === "GET") {
     try {
       const [data] = await UsersModel.getAllUsers();
 
@@ -14,21 +29,6 @@ export default async function handler(req, res) {
       res.status(500).json({
         status: 500,
         message: "GET all users gagal",
-        serverMessage: error,
-      });
-    }
-  } else if (req.method === "POST") {
-    const { body } = req;
-    try {
-      await UsersModel.createNewUser(body);
-      res.status(201).json({
-        status: 201,
-        message: "CREATE new user sukses",
-      });
-    } catch (error) {
-      res.status(500).json({
-        status: 500,
-        message: "CREATE new user gagal",
         serverMessage: error,
       });
     }
