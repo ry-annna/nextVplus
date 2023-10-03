24d3d20c31a37c2427ca3beae93f9f39fa25148a
SuperNayr135 <ryanadytia219@gmail.com>
2023-10-03 20:27:29 +0700

    aada

--- src/services/berita.service.js
+++ src/services/berita.service.js
@@ -2,10 +2,7 @@ import axios from "axios";
 
 export const getAllBerita = async (callback) => {
   try {
-    const response = await axios.get(
-      "https://next-vplus.vercel.app/api/berita" ||
-        "http://localhost:3000/api/berita"
-    );
+    const response = await axios.get("http://localhost:3000/api/berita");
 
     callback(response.data.data);
   } catch (error) {
