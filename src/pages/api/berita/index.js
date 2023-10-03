3eeef8dbeed8d9ddd2bbb5433a9f39e24cad2230
SuperNayr135 <ryanadytia219@gmail.com>
2023-10-03 13:45:17 +0700

    adad

--- src/pages/api/berita/index.js
+++ src/pages/api/berita/index.js
@@ -36,7 +36,7 @@ const corsOptions = {
 };
 
 router
-
+  .use(cors({ credentials: true, origin: "http://localhost:3000" }))
   .get(async (req, res) => {
     try {
       const [data] = await UsersModel.getAllBerita();
