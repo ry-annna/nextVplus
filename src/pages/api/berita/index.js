4f23cf093cd31ff797630ca5e184e631aa1408af
SuperNayr135 <ryanadytia219@gmail.com>
2023-10-03 21:39:48 +0700

    adad

--- src/pages/api/berita/index.js
+++ src/pages/api/berita/index.js
@@ -43,6 +43,12 @@ const corsConfig = {
 
 router
   // .use(cors(corsOptions))
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
   .get(async (req, res) => {
     // const origin = req.headers.get("origin");
     try {
